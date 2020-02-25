130b4031c62b5c81c5139b5b053e4c84ce91bc78
jiselectric9320 <jiselectric9320@gmail.com>
2020-02-26 02:30:06 +0900

    add register feature

--- app.py
+++ app.py
@@ -1,24 +1,13 @@
 from flask import Flask, render_template, request
-from flask_sqlalchemy import SQLAlchemy
-
+import pickle as pk
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///example.sqlite"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+try:
+    f = open('user.db', 'rb')
+    users = pk.load(f)
+    f.close()
+except:
+    users = {}
 
-
-# Create User Table
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    password = db.Column(db.String(128))
-    def __init__(self, id, password):
-        self.id = id
-        self.password = password
-
-
-
-# example data sets
-USERS = {'jikim' : '1234'}
 
 # Login Page
 @app.route('/')
@@ -31,28 +20,36 @@ def check():
     id = request.form.get('id')
     pw = request.form.get('pw')
 
-    if id in USERS and pw == USERS[id]:
-        return 'correct'
-    elif id in USERS and pw != USERS[id]:
-        return 'wrongPW'
+    if id not in users:
+        return 'Account Not Found!'
+    elif users[id] != pw:
+        return 'Password Not Match'
     else:
-        return 'wrong'
-
+        return 'success'
 
 # Login Successful
 @app.route('/success')
 def success():
     return render_template('success.html')
 
-# Register Form
-@app.route('/register', methods=['GET', 'POST'])
+# Account Register
+@app.route('/register')
 def register():
-    if register.method == 'POST':
-        new_user = User(id=request.form['id'], password=request.form['password'])
-        db.session.add(new_user)
-        db.session.commit()
-        return render_template('register.html')
     return render_template('register.html')
+
+@app.route('/regist', methods=["POST"])
+def regist():
+    id = request.form.get("id")
+    pw = request.form.get("pw")
+
+    if id not in users:
+        users[id] = pw
+        f = open("user.db", "wb")
+        pk.dump(users, f)
+        f.close()
+        return "Account Created"
+    else:
+        return "Account Already Exists"
 
 # Error Handler
 app.run()
